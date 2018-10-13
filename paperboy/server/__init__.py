e7f29fc16b03cc8dbf1d4117f35948ac40f7b1d9
Tim Paine <t.paine154@gmail.com>
2018-10-13 09:08:39 -0400

    .

--- paperboy/server/__init__.py
+++ paperboy/server/__init__.py
@@ -1,10 +1,11 @@
+import os
 from .api import FalconAPI
 from .deploy import FalconGunicorn
 
 
 def main():
     options = {
-        'bind': '0.0.0.0:8080',
+        'bind': '0.0.0.0:{}'.format(os.environ.get('PORT', '8080')),
         'workers': 2
     }
     FalconGunicorn(FalconAPI(), options).run()
