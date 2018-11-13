26dda0f6fac8f930471a59ee88a84a98dfc76812
Tim Paine <t.paine154@gmail.com>
2018-11-13 16:20:25 -0500

    fixes #24, more work needed

---
+++ paperboy/output/sqla.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from paperboy.storage.sqla.models.output import OutputSQL
+from .base import BaseOutput
+
+
+class SQLAOutput(BaseOutput):
+    def __init__(self, config, *args, **kwargs):
+        self.config = config
+        self.engine = create_engine(self.storage.sql_url, echo=False)
+        self.sessionmaker = sessionmaker(bind=self.engine)
+
+    def write(self, report, output, *args, **kwargs):
+        task_id = kwargs.get('task_id', '')
+        name = task_id + '_' + datetime.now().strftime('%m-%d-%Y_%H-%M-%S')
+
+        if report['meta']['output'] == 'notebook':
+            name += '.ipynb'
+        elif report['meta']['output'] == 'script':
+            name += '.py'
+        elif report['meta']['output'] == 'email':
+            name += '.eml'
+        elif report['meta']['output'] in ('pdf', 'html'):
+            name += '.{}'.format(report['meta']['output'])
+
+        with self.sessionmaker() as session:
+            report_id = int(report['id'])
+            created = datetime.now()
+
+            out = OutputSQL(name=name,
+                            reportId=report_id,
+                            created=created)
+            session.add(out)
+            session.flush()
---
+++ paperboy/storage/sqla/models/output.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime, LargeBinary
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from .base import Base
+
+
+class OutputSQL(Base):
+    __tablename__ = 'outputs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    reportId = Column(Integer, ForeignKey('reports.id'))
+    report = relationship('ReportSQL', back_populates='outputs')
+
+    created = Column(DateTime)
+    data = LargeBinary()
+
+    def __repr__(self):
+        return "<Output(name='%s')>" % (self.name)
