236fcdd328626f6c51dcaa888335613606bf7be0
Tim Paine <t.paine154@gmail.com>
2018-11-13 16:30:50 -0500

    fixes #29, more testing needed

---
+++ paperboy/storage/sqla/fixtures.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from sqlalchemy import create_engine
+from .models.user import UserSQL
+from .models.notebook import NotebookSQL
+from .models.job import JobSQL
+from .models.report import ReportSQL
+
+
+def main(sql_url):
+    engine = create_engine(sql_url, echo=False)
+    with engine.session() as session:
+        user = UserSQL(name='test')
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+        notebook = '''
+        '''
+        created = datetime.now()
+        modified = datetime.now()
+
+        notebook = NotebookSQL(name='MyNotebook',
+                               userId=user.id,
+                               user=user,
+                               notebook='',
+                               privacy='',
+                               level='',
+                               requirements='',
+                               dockerfile='',
+                               created=created,
+                               modified=modified)
+        session.add(notebook)
+        session.commit()
+        session.refresh(notebook)
+
+        job = JobSQL(name='MyJob',
+                     userId=user.id,
+                     user=user,
+                     notebookId=notebook.id,
+                     notebook=notebook,
+                     start_time=created,
+                     interval='minutely',
+                     level='production',
+                     created=created,
+                     modified=modified)
+        session.add(job)
+        session.commit()
+        session.refresh(job)
+
+        type = 'run'
+        output = 'notebook'
+        strip_code = 'yes'
+
+        created = datetime.now()
+        modified = datetime.now()
+
+        name = job.name + '-Report-1'
+
+        param = '{}'
+        report = ReportSQL(name=name,
+                           userId=user.id,
+                           user=user,
+                           notebookId=notebook.id,
+                           notebook=notebook,
+                           jobId=job.id,
+                           job=job,
+                           type=type,
+                           output=output,
+                           strip_code=strip_code,
+                           parameters=param,
+                           created=created,
+                           modified=modified)
+        session.add(report)
+        session.commit()
+        session.refresh(report)
