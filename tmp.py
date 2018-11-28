d6cbd816751d37bf344756c5a6de72b401d31e1f
Tim Paine <t.paine154@gmail.com>
2018-11-28 17:33:32 -0500

    .

---
+++ tmp.py
@@ -0,0 +1,61 @@
+import os
+import os.path
+import sys
+# dokku_source = dokku@host1.paine.nyc
+# name -> report_name.jobid
+
+
+def launch(notebook_string, name, dokku_source):
+    directory = new_directory()
+    make_voila_proj(notebook_string, name, directory)
+    remote = make_dokku_proj(directory, name, dokku_source)
+    dokku_deploy(remote)
+
+
+def new_directory():
+    try:
+        from tempdir import TemporaryDirectory
+    except ImportError:
+        from backports.tempfile import TemporaryDirectory
+    return TemporaryDirectory()
+
+
+def make_voila_proj(notebook, name, directory, requirements=''):
+    with open(os.path.join(directory, name), 'w') as fp:
+        fp.write(notebook)
+
+    with open(os.path.join(directory, 'run.py'), 'w') as fp:
+        fp.write('''
+import os
+from voila.app import Voila
+
+def launch_voila():
+    v = Voila()
+    v.notebook_path = os.path.dirname(__file__)
+    v.launch_instance()
+
+if __name__ == '__main__':
+    launch_voila()
+''')
+
+    with open(os.path.join(directory, 'Procfile'), 'w') as fp:
+        fp.write('web: {} -m run.py'.format(sys.executable))
+
+    if requirements:
+        with open(os.path.join(directory, 'requirements.txt'), 'w'):
+            fp.write(requirements)
+
+
+def make_dokku_proj(directory, name, dokku_source):
+    from git import Repo
+    repo = Repo.init(directory)
+
+    track = [f for f in os.listdir('.') if f not in ('.git')]
+    repo.index.add(track)
+    repo.index.commit('Readying repo for deploy')
+    remote = repo.create_remote('dokku', url='{}:{}'.format(dokku_source, name))
+    return remote
+
+
+def dokku_deploy(remote):
+    remote.push(refspec='master:master')
