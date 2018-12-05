7b2936ffbd7f0e4f890ec0cb0c3151da80b0aea2
Tim Paine <t.paine154@gmail.com>
2018-12-05 09:44:22 -0500

    working on voila deploy

--- tmp.py
+++ tmp.py
@@ -1,12 +1,12 @@
 import os
 import os.path
-import sys
 
 with open('./examples/sample.ipynb', 'r') as fp:
     NOTEBOOK = fp.read()
 
-NAME = 'test.job1'
+NAME = 'test-job1'
 DOKKU_SRC = 'dokku@host1.paine.nyc'
+APP_ROOT = 'app.paine.nyc'
 
 
 def launch(notebook_string, name, dokku_source):
@@ -37,7 +37,7 @@ def make_voila_proj(notebook, name, directory, requirements=''):
     with open(os.path.join(directory, name + '.ipynb'), 'w') as fp:
         fp.write(notebook)
 
-    with open(os.path.join(directory, 'run.py'), 'w') as fp:
+    with open(os.path.join(directory, 'run_job.py'), 'w') as fp:
         fp.write('''
 import os
 from voila.app import Voila
@@ -45,14 +45,15 @@ from voila.app import Voila
 def launch_voila():
     v = Voila()
     v.notebook_path = os.path.join(os.path.dirname(__file__), '{}') + '.ipynb'
+    v.port = int(os.environ.get('PORT', 8866))
     v.launch_instance()
 
 if __name__ == '__main__':
     launch_voila()
-'''.join(name))
+'''.format(name))
 
     with open(os.path.join(directory, 'Procfile'), 'w') as fp:
-        fp.write('web: {} -m run.py'.format(sys.executable))
+        fp.write('web: python3 -m run_job.py')
 
     if not requirements:
         requirements = '''voila'''
@@ -67,7 +68,7 @@ def make_dokku_proj(directory, name, dokku_source):
     track = [f for f in os.listdir(directory) if f not in ('.git')]
     repo.index.add(track)
     repo.index.commit('Readying repo for deploy')
-    remote = repo.create_remote('dokku', url='{}:{}'.format(dokku_source, name))
+    remote = repo.create_remote('dokku', url='{}:{}.{}'.format(dokku_source, name, APP_ROOT))
     return remote
 
 
