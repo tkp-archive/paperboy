85611c9a1a61a6a32e4536ceed0991d7fd023d11
Tim Paine <t.paine154@gmail.com>
2018-12-30 01:40:15 +0000

    .

--- paperboy/middleware/__init__.py
+++ paperboy/middleware/__init__.py
@@ -1,15 +1,15 @@
 # CORS middleware
-from falcon_cors import CORS as CORSMiddleware
+from falcon_cors import CORS as CORSMiddleware  # noqa: F401
 
 # Authentication middlewares
-from falcon_helpers.middlewares.auth_required import AuthRequiredMiddleware
-from falcon_helpers.middlewares.load_user import LoadUserMiddleware
+from falcon_helpers.middlewares.auth_required import AuthRequiredMiddleware  # noqa: F401
+from falcon_helpers.middlewares.load_user import LoadUserMiddleware  # noqa: F401
 
 # Form handling middleware
-from falcon_multipart.middleware import MultipartMiddleware
+from falcon_multipart.middleware import MultipartMiddleware  # noqa: F401
 
 # No auth
-from .none import NoUserMiddleware, NoAuthRequiredMiddleware
+from .none import NoUserMiddleware, NoAuthRequiredMiddleware  # noqa: F401
 
 # SQL
-from .sqla import SQLUserMiddleware, SQLAuthRequiredMiddleware, SQLAlchemySessionMiddleware
+from .sqla import SQLUserMiddleware, SQLAuthRequiredMiddleware, SQLAlchemySessionMiddleware  # noqa: F401
