df9d402d0fad4b3ceabc10ea624e478296473e23
Tim Paine <t.paine154@gmail.com>
2019-02-14 16:35:59 -0500

    fixing tests

--- tests/ts/export.test.ts
+++ tests/ts/export.test.ts
@@ -1,6 +1,6 @@
 import "isomorphic-fetch";
 
-import * as extension from '../../ts/src/index';
+import * as extension from '../../src/ts/index';
 
 describe('Checks exports', () => {
   test("Check extension", () => {
